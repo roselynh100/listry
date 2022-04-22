4c9260e6527b50b9cf90c4b2f9295091abea6bc8
Jason Wang <jason.wang1700@gmail.com>
2022-04-22 10:14:08 -0400

    added more pages and put map onto separate page

--- frontend/src/components/home/index.jsx
+++ frontend/src/components/home/index.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../home/home.scss";
 
 export default function Home() {
-	return (
-		<div className="homeContainer">
-			<h3>How would you like to enter your shopping list today?</h3>
-            <button>Take picture</button>
-            <button>Manually input</button>
-		</div>
-	);
+    return (
+        <div className="homeContainer">
+            <h3>How would you like to enter your shopping list today?</h3>
+            <Link to="/camera">
+                <button>Take picture</button>
+            </Link>
+
+            <Link to="/checklist">
+                <button>Manually input</button>
+            </Link>
+        </div>
+    );
 }
