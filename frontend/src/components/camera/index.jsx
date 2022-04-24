96b9079cbf8228e15ced9f2a28991fb648ef5232
Jason Wang <jason.wang1700@gmail.com>
2022-04-23 23:18:33 -0400

    started working on image detection

--- frontend/src/components/camera/index.jsx
+++ frontend/src/components/camera/index.jsx
@@ -1,25 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { createWorker } from "tesseract.js";
 import "./camera.scss";
 
 export default function Camera() {
-	const [image, setImage] = useState();
+    const [ocr, setOcr] = useState("");
+    const [image, setImage] = useState();
+    const worker = createWorker();
 
-	const getImage = (event) => {
-		const imageHolder = event.target.files[0];
-		setImage(URL.createObjectURL(imageHolder));
-	};
-    
-	return (
-		<div>
-			<input
-				type="file"
-				accept="image/*"
-				onChange={(event) => {
-					getImage(event);
-				}}
-			/>
-			{image && <img src={image} />}
-		</div>
-	);
+    const convertImageToText = async () => {
+        if (!image) return;
+        await worker.load();
+        await worker.loadLanguage("eng");
+        await worker.initialize("eng");
+        const {
+            data: { text },
+        } = await worker.recognize(image);
+        setOcr(text);
+    };
+
+    useEffect(() => {
+        convertImageToText();
+    }, [image]);
+
+    function handleImageChange(event) {
+        const imageHolder = event.target.files[0];
+        if(!imageHolder)return;
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          const imageUri = reader.result;
+          setImage(imageUri);
+        };
+        reader.readAsDataURL(imageHolder);
+      }
+
+    return (
+        <div>
+            <input
+                type="file"
+                accept="image/*"
+                onChange={handleImageChange}
+            />
+            {image && <img src={image} className="grocery-image"/>}
+            <p>{ocr}</p>
+        </div>
+    );
 }
 
