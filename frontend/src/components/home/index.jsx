f3161f6470e2e8f5794f5b2e18791de2bd955cad
Jason Wang <jason.wang1700@gmail.com>
2022-05-02 13:25:23 -0400

    adjusted css for home page

--- frontend/src/components/home/index.jsx
+++ frontend/src/components/home/index.jsx
@@ -6,14 +6,16 @@ import { ItemContext } from "../../contexts/listContext";
 export default function Home() {
 	return (
 		<div className="homeContainer">
-			<h3>How would you like to enter your shopping list today?</h3>
-			<Link className="navigate" to="/camera">
-				<button className="navigateButton">Take picture</button>
-			</Link>
+			<div className="homeContent">
+				<h3>How would you like to enter your shopping list today?</h3>
+				<Link className="navigate" to="/camera">
+					<button className="navigateButton">Take picture</button>
+				</Link>
 
-			<Link className="navigate" to="/checklist">
-				<button className="checklistButton">Manually input</button>
-			</Link>
+				<Link className="navigate" to="/checklist">
+					<button className="checklistButton">Manually input</button>
+				</Link>
+			</div>
 		</div>
 	);
 }
