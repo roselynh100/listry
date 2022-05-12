0305e834f3644f51b49eb3c43080ca7bc54f68b6
Roselyn <roselynh100@gmail.com>
2022-05-12 00:20:38 -0400

    Edited front end for home page

--- frontend/src/components/home/index.jsx
+++ frontend/src/components/home/index.jsx
@@ -3,9 +3,12 @@ import { Link } from "react-router-dom";
 import "./home.scss";
 import { ItemContext } from "../../contexts/listContext";
 
+import Header from "../header";
+
 export default function Home() {
 	return (
 		<div className="homeContainer">
+			<Header />
 			<div className="homeContent">
 				<h3>How would you like to enter your shopping list today?</h3>
 				<Link className="navigate" to="/camera">
